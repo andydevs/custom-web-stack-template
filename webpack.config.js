d0c98df3fd56f9a924a15761aa6c92998f7931f7
Anshul Kharbanda <akanshul97@gmail.com>
2020-11-08 03:17:11 -0500

    Implemented webpack for babel and SCSS

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+/**
+ * Custom stack template stuff
+ * 
+ * Author:  Anshul Kharbanda
+ * Created: [Creation Date]
+ */
+const webpack = require('webpack')
+const path = require('path')
+
+// Webpack config module
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s?css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
