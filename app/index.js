47c8c2009576779d61fd60dc58c29ac597716d3e
Anshul Kharbanda <akanshul97@gmail.com>
2020-11-10 01:31:16 -0500

    Added a form n stuff

--- app/index.js
+++ app/index.js
@@ -11,8 +11,23 @@ import car from './assets/images/lincoln-navigator.jpg';
 console.log('yoyoyoyoyo')
 
 // Add image
-let app = document.getElementById('app')
-let img = document.createElement('img')
-img.src = car
-img.classList.add('car')
-app.appendChild(img)
+let putImage = document.getElementById('put-image')
+let image = document.createElement('img')
+image.src = car
+image.classList.add('car')
+putImage.appendChild(image)
+
+let form = document.getElementById('feedback')
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    // Get data
+    let name = form.querySelector('input[name="name"]').value
+    let thoughts = form.querySelector('textarea[name="thoughts"]').value
+    let like = form.querySelector('input[name="like"]').checked
+
+    // Put data in element
+    let entry = document.createElement('tr')
+    entry.innerHTML = `<td>${name}</td><td>${thoughts}</td><td>${like}</td>`
+    document.getElementById('submissions-entry').appendChild(entry)
+})
